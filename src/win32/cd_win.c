3fc69153fc8728d74b57af27218b3338581c32dc
apartfromtime <42292382+apartfromtime@users.noreply.github.com>
2021-01-26 22:39:52 +1100

    Fix compiler warning
    Signed unsigned mismatch.

--- src/win32/cd_win.c
+++ src/win32/cd_win.c
@@ -382,7 +382,7 @@ static void CD_f (void)
 
 LONG CDAudio_MessageHandler(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-	if (lParam != wDeviceID)
+	if ((UINT)lParam != wDeviceID)
 		return 1;
 
 	switch (wParam)
