a190c18bea13af76214521febe6ec993222e81c0
apartfromtime <42292382+apartfromtime@users.noreply.github.com>
2021-02-12 17:08:06 +1100

    Fix compiler warning
    Nonstandard extension, function/data pointer conversion in expression.

--- src/win32/rw_win.h
+++ src/win32/rw_win.h
@@ -26,7 +26,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 typedef struct
 {
 	HINSTANCE		hInstance;
-	void			*wndproc;
+	WNDPROC wndproc;
 	HDC				hDC;				// global DC we're using
 	HWND			hWnd;				// HWND of parent window
 
